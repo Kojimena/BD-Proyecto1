fd35f6118e7364565b993173cb665d5de52eb241
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2023-02-28 16:02:04 -0600

    Update etapa_2.sql

--- data/etapa_2.sql
+++ data/etapa_2.sql
@@ -1,9 +1,26 @@
+-- Cantidad de juegos jugados por cada equipo
 select team_long_name, count(*) as played
 from team
 inner join match m on team.team_api_id = m.home_team_api_id or team.team_api_id = m.away_team_api_id
 where team_api_id = m.home_team_api_id or team_api_id = m.away_team_api_id
 group by team_long_name
 order by played desc;
+
+-- goles a favor, gol en contra, diferencia de goles
+SELECT
+  league.name_league,
+  match.season,
+  team.team_long_name AS equipo,
+  SUM(CASE WHEN match.home_team_api_id = team.team_api_id THEN match.home_team_goal ELSE match.away_team_goal END) AS goles_a_favor,
+  SUM(CASE WHEN match.home_team_api_id = team.team_api_id THEN match.away_team_goal ELSE match.home_team_goal END) AS goles_en_contra,
+  SUM(CASE WHEN match.home_team_api_id = team.team_api_id THEN match.home_team_goal ELSE match.away_team_goal END) -
+    SUM(CASE WHEN match.home_team_api_id = team.team_api_id THEN match.away_team_goal ELSE match.home_team_goal END) AS diferencia_de_goles
+FROM match
+JOIN team ON match.home_team_api_id = team.team_api_id OR match.away_team_api_id = team.team_api_id
+JOIN league ON match.league_id = league.id
+GROUP BY league.name_league, match.season, team.team_long_name
+ORDER BY league.name_league, match.season, equipo;
+
 
 -- Cantidad de juegos ganados
 with partidos as (
